da26e2b0c56cbe15ae8ed296a683421db44b8fc7
Michael Gillett <gillemic@flip2.engr.oregonstate.edu>
2017-01-27 14:39:00 -0800

    Shuffle through points and create lists

--- Assign1/bruteForce.py
+++ Assign1/bruteForce.py
@@ -1,2 +1,20 @@
-#Struct with x and y val
-#Nathan sux
+import As1HelperFunctions
+from math import sqrt
+
+p = As1HelperFunctions.getMyPoints()
+
+coords = []
+dists = []
+for i in range(0, len(p)):
+   for j in range(i+1, len(p)):
+      y = abs(p[j][1] - p[i][1])
+      x = abs(p[j][0] - p[i][0])
+
+      d = sqrt((y*y) + (x*x))
+      
+      coords.append([p[i], p[j]])
+      dists.append(d)
+
+indices = [i for i, x in enumerate(dists) if x == min(dists)]
+
+#for z in range(0, len(coords)):
