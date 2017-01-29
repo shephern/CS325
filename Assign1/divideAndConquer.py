8bc080f3c900d6419873d80ed2e5f27900e8fb02
DrakeSeifert <drakeseifert1@gmail.com>
2017-01-28 17:10:12 -0800

    More changes to D&C

--- Assign1/divideAndConquer.py
+++ Assign1/divideAndConquer.py
@@ -1,5 +1,5 @@
 #1. split into two halves by x-coordinate
-#2. Recursively compute closets pair in each half
+#2. Recursively compute closest pair in each half
 #3. Solve merge step based on above results
 #4. Determine set of points whose x-coords lie in
 #   range of [Xm - d, Xm + d] where Xm is middle
@@ -10,3 +10,34 @@
 #   differs from p by at most d
 #7. return overall min distance
 
+import As1HelperFunctions
+from math import sqrt
+testing = True
+
+p = As1HelperFunctions.getMyPoints()
+
+sortByX = sorted(p, key=lambda x: x[1])
+
+firstHalf = []
+secHalf = []
+
+for i in range(0, len(sortByX)/2):
+	firstHalf.append(sortByX[i])
+
+for i in range(len(sortByX)/2, len(sortByX)):
+	secHalf.append(sortByX[i])
+
+if(testing):
+	print "sortByX:\n",sortByX
+	print "firstHalf:\n",firstHalf
+	print "secHalf:\n",secHalf
+
+def DandC(coords):
+	print "" #so it compiles
+	#algorith here (this is actually hard)
+	#if base case
+	#	solve and return
+	#else do stuff
+	#	DandC(coords/2)
+
+#As1HelperFunctions.createOutputFile(min_dist, pairs, 1)
