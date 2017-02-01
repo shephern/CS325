413df34737124384704aaf5c70c1b9b644f10d6c
DrakeSeifert <drakeseifert1@gmail.com>
2017-01-31 20:22:45 -0800

    DandC basically finished, need to add coordinate pairs

--- Assign1/divideAndConquer.py
+++ Assign1/divideAndConquer.py
@@ -12,29 +12,29 @@
 
 import As1HelperFunctions
 from math import sqrt
-testing = True
+testing = False
 
 p = As1HelperFunctions.getMyPoints()
+sortByX = sorted(p, key=lambda x: x[0])
 
-sortByX = sorted(p, key=lambda x: x[1])
+if(testing):
 
-# if(testing):
+	#print len([1,2,3,4,5])/2
 
-# 	print len([1,2,3,4,5])/2
+	firstHalf = []
+	secHalf = []
+	for i in range(0, len(sortByX)/2): #Note: len(coords)/2 rounds down for odd numbers
+		firstHalf.append(sortByX[i])
+	for i in range(len(sortByX)/2, len(sortByX)):
+		secHalf.append(sortByX[i])
 
-# 	firstHalf = []
-# 	secHalf = []
-# 	for i in range(0, floor(len(sortByX)/2)):
-# 		firstHalf.append(sortByX[i])
-# 	for i in range(floor(len(sortByX)/2)), len(sortByX)):
-# 		secHalf.append(sortByX[i])
+	print "sortByX:\n",sortByX
+	print "firstHalf:\n",firstHalf
+	print "secHalf:\n",secHalf
 
-# 	print "sortByX:\n",sortByX
-# 	print "firstHalf:\n",firstHalf
-# 	print "secHalf:\n",secHalf
-
+shortestPoints = [];
 def DandC(coords):
-	#Base Case: Only two points in section
+	#Base Case: less than 4 points in list
 	#Find distance
 	#Recurse back, compare recursed distances
 	#Check points outside dividing line by min distance
@@ -51,6 +51,15 @@ def DandC(coords):
 				   sqrt(pow(coords[2][0]-coords[0][0], 2) + pow(coords[2][1]-coords[0][1], 2)) )
 
 	else:
+		#Compute separation line:
+		if(len(coords)%2 == 0): #If even number of coordinates
+			#Take 2 middle x values, find difference, divide by two, add to smallest x value
+			midLine = ( float((coords[len(coords)/2][0] - \
+						coords[(len(coords)/2)-1][0]) / 2) + \
+						coords[(len(coords)/2)-1][0] )
+		else: #else odd number of coordinates
+			midLine = coords[(len(coords)/2)][0]
+
 		#Split list into two halves
 		firstHalf = []
 		secHalf = []
@@ -62,17 +71,39 @@ def DandC(coords):
 		#Recurse
 		firstHalfDistance = DandC(firstHalf)
 		secHalfDistance = DandC(secHalf)
-		if (firstHalfDistance <= secHalfDistance):
-			minDist = firstHalfDistance
-		else:
-			minDist = secHalfDistance
 
 		#minDist = (firstHalfDistance if (firstHalfDistance <= secHalfDistance) else secHalfDistance)
+		minDist = min(firstHalfDistance, secHalfDistance)
 
-		return minDist
-		#Check points around dividing line
+		#Check around separation line
+		closePoints = []
+		lineRange = [midLine-minDist, midLine+minDist]
+		for i in range(0, len(coords)):
+			if(coords[i][0] >= lineRange[0] and \
+			coords[i][0] <= lineRange[1]): #if the point is within range
+				closePoints.append(coords[i])
+
+		#if nothing to compare, return
+		if (len(closePoints) < 2):
+			return minDist
+
+		#Split list into two halves
+		midLinefirstHalf = []
+		midLinesecHalf = []
+		for i in range(0, len(closePoints)/2):
+			midLinefirstHalf.append(closePoints[i])
+		for i in range(len(closePoints)/2, len(closePoints)):
+			midLinesecHalf.append(closePoints[i])
+
+		#More Recursion!
+		midLinefirstHalfDistance = DandC(midLinefirstHalf)
+		midLinesecHalfDistance = DandC(midLinesecHalf)
+
+		#otherMinDist = (firstHalfDistance if (firstHalfDistance <= secHalfDistance) else secHalfDistance)
+		midLineMinDist = min(midLinefirstHalfDistance, midLinesecHalfDistance)
+
+		return min(minDist, midLineMinDist)
 
 shortestDistance = DandC(sortByX)
 print shortestDistance
-
-#As1HelperFunctions.createOutputFile(min_dist, pairs, 1)
+#As1HelperFunctions.createOutputFile(shortestDistance, pairs, 1)
