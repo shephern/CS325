e36e707e557dd764f48d48d98e329dda349f5869
DrakeSeifert <drakeseifert1@gmail.com>
2017-01-31 20:34:56 -0800

    DandC basically finished, still need to add coordinate pairs

--- Assign1/divideAndConquer.py
+++ Assign1/divideAndConquer.py
@@ -1,38 +1,9 @@
-#1. split into two halves by x-coordinate
-#2. Recursively compute closest pair in each half
-#3. Solve merge step based on above results
-#4. Determine set of points whose x-coords lie in
-#   range of [Xm - d, Xm + d] where Xm is middle
-#   line and d is currently found min distance
-#5. Order the points in ascending order of their
-#   y-coords
-#6. Compare each point p to only those whose y-coord
-#   differs from p by at most d
-#7. return overall min distance
-
 import As1HelperFunctions
 from math import sqrt
-testing = False
 
-p = As1HelperFunctions.getMyPoints()
-sortByX = sorted(p, key=lambda x: x[0])
+sortByX = sorted(As1HelperFunctions.getMyPoints(), key=lambda x: x[0])
+#shortestPoints = [];
 
-if(testing):
-
-	#print len([1,2,3,4,5])/2
-
-	firstHalf = []
-	secHalf = []
-	for i in range(0, len(sortByX)/2): #Note: len(coords)/2 rounds down for odd numbers
-		firstHalf.append(sortByX[i])
-	for i in range(len(sortByX)/2, len(sortByX)):
-		secHalf.append(sortByX[i])
-
-	print "sortByX:\n",sortByX
-	print "firstHalf:\n",firstHalf
-	print "secHalf:\n",secHalf
-
-shortestPoints = [];
 def DandC(coords):
 	#Base Case: less than 4 points in list
 	#Find distance
